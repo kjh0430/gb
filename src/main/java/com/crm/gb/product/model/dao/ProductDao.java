c5798d4384099dfde4fe7096ecdc81b1103fd75d
user <user@DESKTOP-JM2RC9F>
2018-05-15 16:19:26 +0900

    Product vo,dao,service,serviceimpl 추가

--- src/main/java/com/crm/gb/product/model/dao/ProductDao.java
+++ src/main/java/com/crm/gb/product/model/dao/ProductDao.java
@@ -1,5 +1,18 @@
 package com.crm.gb.product.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.crm.gb.product.model.vo.Product;
+
+@Repository("ProductDao")
 public class ProductDao {
+
+	private SqlSessionTemplate sqlSession;
+	
+	public int insertProduct(Product product) {
+		// 제품등록
+		return sqlSession.insert("insertProduct",product);
+	}
 
 }
--- src/main/java/com/crm/gb/product/model/service/ProductService.java
+++ src/main/java/com/crm/gb/product/model/service/ProductService.java
@@ -2,7 +2,11 @@ package com.crm.gb.product.model.service;
 
 import org.springframework.stereotype.Service;
 
+import com.crm.gb.product.model.vo.Product;
+
 @Service
 public interface ProductService {
+	
+	int insertProduct(Product product);
 
 }
