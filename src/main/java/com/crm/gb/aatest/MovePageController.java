5ffb004eba73c9f45de7ceefadfdb5930fa688bd
user01 <user01@user01>
2018-05-16 10:59:39 +0900

    emp 수정

--- src/main/java/com/crm/gb/aatest/MovePageController.java
+++ src/main/java/com/crm/gb/aatest/MovePageController.java
@@ -1,4 +1,4 @@
-package com.crm.gb.aatest;
+/*package com.crm.gb.aatest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -201,3 +201,4 @@ public class MovePageController {
 		
 		
 }
+*/
