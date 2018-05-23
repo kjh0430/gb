fb75b0dcb0d346a3590bc996a857545d9726c905
alway <alway@192.168.137.121>
2018-05-23 14:15:22 +0900

    jh commit

--- src/main/java/com/crm/gb/approval/controller/ApprovalController.java
+++ src/main/java/com/crm/gb/approval/controller/ApprovalController.java
@@ -2,6 +2,8 @@ package com.crm.gb.approval.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ApprovalController {
@@ -14,6 +16,15 @@ public class ApprovalController {
 		return "approval/approval";
 	}
 	
+	
+	@RequestMapping(value="getName.do", method=RequestMethod.GET)
+	public void getName(@RequestParam(name="emp_no") int emp_no) {
+		System.out.println("emp_no"+emp_no);
+	
+		
+		
+	
+	}
 	
 	
 }
