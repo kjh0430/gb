6fa34e08b965912f3fd5a80e4c369e29a8cd87f4
alway <alway@192.168.137.121>
2018-05-24 12:10:40 +0900

    jh commit

--- src/main/java/com/crm/gb/approval/model/vo/Approval.java
+++ src/main/java/com/crm/gb/approval/model/vo/Approval.java
@@ -18,8 +18,18 @@ public class Approval implements Serializable {
 	private String approval_team_date;
 	private String approval_mgr_date;
 	
+	//팀장, 관리자 이름을 가져오기 위한 vo 추가
+	private String team_mgr_name;
+	private String mgr_name;
+	
+	
+	
+	public Approval() {}
+	
+
 	public Approval(int emp_no, String approval_choose_no, String approval_submit_date, String approval_start_date,
-			String approval_end_date, String approval_comment, String approval_team_date, String approval_mgr_date) {
+			String approval_end_date, String approval_comment, String approval_team_date, String approval_mgr_date,
+			String team_mgr_name, String mgr_name) {
 		super();
 		this.emp_no = emp_no;
 		this.approval_choose_no = approval_choose_no;
@@ -29,6 +39,8 @@ public class Approval implements Serializable {
 		this.approval_comment = approval_comment;
 		this.approval_team_date = approval_team_date;
 		this.approval_mgr_date = approval_mgr_date;
+		this.team_mgr_name = team_mgr_name;
+		this.mgr_name = mgr_name;
 	}
 
 	public int getEmp_no() {
@@ -98,14 +110,37 @@ public class Approval implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+	
+	
+
+	public String getTeam_mgr_name() {
+		return team_mgr_name;
+	}
+
+	public void setTeam_mgr_name(String team_mgr_name) {
+		this.team_mgr_name = team_mgr_name;
+	}
+
+	public String getMgr_name() {
+		return mgr_name;
+	}
+
+	public void setMgr_name(String mgr_name) {
+		this.mgr_name = mgr_name;
+	}
 
 	@Override
 	public String toString() {
 		return "Approval [emp_no=" + emp_no + ", approval_choose_no=" + approval_choose_no + ", approval_submit_date="
 				+ approval_submit_date + ", approval_start_date=" + approval_start_date + ", approval_end_date="
 				+ approval_end_date + ", approval_comment=" + approval_comment + ", approval_team_date="
-				+ approval_team_date + ", approval_mgr_date=" + approval_mgr_date + "]";
+				+ approval_team_date + ", approval_mgr_date=" + approval_mgr_date + ", team_mgr_name=" + team_mgr_name
+				+ ", mgr_name=" + mgr_name + "]";
 	}
+
+
+	
+	
 	
 	
 	
