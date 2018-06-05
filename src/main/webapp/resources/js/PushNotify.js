16eb82b628a776ada48f32930f9422b829f22b09
user <user@DESKTOP-JM2RC9F>
2018-06-05 10:17:25 +0900

    notify.js

---
+++ src/main/webapp/resources/js/PushNotify.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+var ws=null;
+$(function(){
+	//console.log("되고있니?");
+	
+	ws = new WebSocket("ws://localhost:7300/gb/hello.do?emp_no="+emp_no);
+    ws.onopen = function () {
+       console.log("onopen");
+    };
+    ws.onmessage = function (event) {
+    	console.log("onmessage" + event.data);
+    		
+		 var img = 'resources/images/apr.png';
+		 var text = event.data+"님이 결재를 신청하셨습니다.";
+		
+		 if (window.Notification && Notification.permission === "granted") {
+			var notification = new Notification('Message', { body: text, icon: img });
+		 }else if(mNotify =="mobile"){
+   			alert(text);
+   		}		
+		
+    };
+    ws.onclose = function (event) {
+    	console.log("onclose");
+    };
+
+	
+})
+
